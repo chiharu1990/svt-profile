3f19faf6a4c6615530f35ee670335cef58fb30d8
chiharu1990 <chiharufukuyama.1990@gmail.com>
2025-02-01 15:04:24 +0900

    一度TOPにデザイン通りに表示させてみる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,83 +1,60 @@
 import Image from 'next/image';
+import jsonData from '../jsonData.json';
+import { pathwayGothicOne } from '../../utils/font';
+
+type Member = {
+    name: string;
+    jpName: string;
+    realName: string;
+    jpRealName: string;
+    nickName: string[];
+    emoji: string;
+    birthday: string;
+};
+
+type MemberType = {
+    seventeen: Member[];
+};
 
 export default function Home() {
+    const data: MemberType = jsonData;
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <Image
-                    className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
-                    priority
-                />
-                <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-                    <li className="mb-2">
-                        Get started by editing{' '}
-                        <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-                            src/app/page.tsx
-                        </code>
-                        .
-                    </li>
-                    <li>Save and see your changes instantly.</li>
-                </ol>
-
-                <div className="flex gap-4 items-center flex-col sm:flex-row">
-                    <a
-                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark:invert"
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={20}
-                            height={20}
-                        />
-                        Deploy now
-                    </a>
-                    <a
-                        className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Read our docs
-                    </a>
+        <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20">
+            <h2 className={`${pathwayGothicOne.className} text-6xl mb-12 text-blue`}>MEMBER</h2>
+            {data.seventeen.map((item) => (
+                <div key={item.name} className="grid grid-cols-2 gap-4 mb-10">
+                    <Image src="/images/dummy.png" alt="image" width={300} height={300}></Image>
+                    <div>
+                        <div className="mb-4">
+                            <p className={`${pathwayGothicOne.className} text-5xl`}>
+                                {item.name}
+                                <span className="text-2xl ml-4">{item.jpName}</span>
+                            </p>
+                        </div>
+                        <div className="mb-4">
+                            <h4 className="text-base font-bold mb-1">本名</h4>
+                            <p className="text-2xl">
+                                {item.realName}
+                                <span className="ml-4">{item.jpRealName}</span>
+                            </p>
+                        </div>
+                        <div className="flex mb-4">
+                            <div>
+                                <h4 className="text-base font-bold mb-1">ニックネーム</h4>
+                                <p className="text-2xl">{item.nickName}</p>
+                            </div>
+                            <div className="ml-6">
+                                <h4 className="text-base font-bold mb-1">絵文字</h4>
+                                <p className="text-2xl">{item.emoji}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <h4 className="text-base font-bold mb-1">誕生日</h4>
+                            <p className="text-2xl">{item.birthday}</p>
+                        </div>
+                    </div>
                 </div>
-            </main>
-            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-                    Learn
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-                    Examples
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-                    Go to nextjs.org →
-                </a>
-            </footer>
+            ))}
         </div>
     );
 }
---
+++ utils/font.ts
@@ -0,0 +1,7 @@
+import { Pathway_Gothic_One } from 'next/font/google';
+
+export const pathwayGothicOne = Pathway_Gothic_One({
+    weight: ['400'],
+    subsets: ['latin'],
+    display: 'swap',
+});
